71eff3a4cc4b30d53af36a3e4b38ba03f6a1f80b
zzmin0405 <116485459+zzmin0405@users.noreply.github.com>
2026-09-16 05:25:03 +0900

    외형 모델 캐시 버전 갱신

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,4 @@
-import { createWorld } from "./world.js?v=chase-fix-2";
+import { createWorld } from "./world.js?v=skins-3";
 import { createNeuronView } from "./neuron-view.js?v=2";
 const $ = (id) => document.getElementById(id),
   canvas = $("game"),
